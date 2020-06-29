62221475525abd86e51e3d77c25180db2e2e9d67
Byron Samaripa <Byron.Samaripa@gmail.com>
2020-06-29 11:15:02 -0600

    Day 002 Palindrome

---
+++ problems/002-valid-palindrome.py
@@ -0,0 +1,19 @@
+import unittest
+from string import ascii_letters
+
+class TestValidPalindrome(unittest.TestCase):
+  def valid_palindrome(self, input: str) -> bool:
+    if len(input) is 0: return False
+    filtered = "".join([ch for ch in input.upper() if ch in ascii_letters])
+    return filtered == filtered[::-1]
+
+  def test_valid_palindrome(self):
+    self.assertEqual(self.valid_palindrome(''), False)
+    self.assertEqual(self.valid_palindrome('FOOF'), True)
+    self.assertEqual(self.valid_palindrome('FOO'), False)
+    self.assertEqual(self.valid_palindrome('level'), True)
+    self.assertEqual(self.valid_palindrome('algorithm'), False)
+    self.assertEqual(self.valid_palindrome('A man, a plan, a canal: Panama.'), True)
+
+if __name__ == '__main__':
+  unittest.main()
